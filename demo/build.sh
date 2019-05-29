8c0db4bbeb4aa5e7fa073664a9ad2952ffcb7afa
redabs <redabs@users.noreply.github.com>
2019-05-29 17:57:22 +0200

    Compile demo with gcc, make build directory in build script

--- demo/build.sh
+++ demo/build.sh
@@ -1,3 +1,4 @@
 (cd ../src/; ./build.sh)
+mkdir -p build
 CFLAGS="-Wall -std=c11 -pedantic -lSDL2 -lGL -O3 -g -Werror=implicit-function-declaration"
-clang $CFLAGS demo.c ../src/ui.o -I../src -o build/demo.bin
+gcc $CFLAGS demo.c ../src/ui.o -I../src -o build/demo.bin
